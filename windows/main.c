bfc245b5e3e2978f0a6bd0b7de8e51e6e48db49d
Esad <eakar2012@fau.edu>
2018-10-18 03:02:24 -0400

    init

--- windows/main.c
+++ windows/main.c
@@ -12,111 +12,78 @@ fl | grep -v -E 'exe|txt' | fw -c make -r demo >log.txt 2>&1
 #include <time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <dirent.h>
 #include <string.h>
 #include <windows.h>
 #include <conio.h>
 #include <process.h>
+#include <file.h>
 
-#define MAXFILES 100
-#define MAXFNAME 100
 #define KEY_ESC 0x1B
-
-typedef struct {
-    char* pname;
-    char* runcall;
-    char* compcall;
-} Params;
-
-Params p;
-char kill[100];
-int is_running;
-HANDLE run_mutex;
+#define KEY_ENT '\r'
 
 // reads input from stdin until EOF or \n
 int _getline(FILE*, char*, int);
-
 // prints program usage if initial parameters are wrong or missing
 void usage();
-
 // main system interface, makes system calls defined by user in initial arguments, seperate thread
-void system_call();
-
+void sys_call();
 // reads keyboard input when -r program is not running, seperate thread
 void input();
+// records file mod time and copies file name to fnames
+void register_file(char* );
+// this gets called at the start, and whenver user wants to change compilation or program name parameters
+void get_commands();
+void print_keys();
+void print_commands();
+
+char fnames[MAX_FILE_PATHS][MAX_FILE_NAME] = {{0}};
+char compile[1000];
+char run[1000];
+char args[1000];
+char kill[1000];
+time_t mtimes[MAX_FILE_PATHS];
+int n;
+char *pname;
+int is_running;
 
 
 int main(int argc, char** argv)
-{
-    char *pname = *argv, *run = NULL, *compile = NULL;
+{   
+    usage();
+    pname = argv[0];
+    n = 0;
+    char new_file[MAX_FILE_NAME];
 
-    // initial arguments processing
-    while (--argc > 0) {
-        if ((*++argv)[0] == '-') {
-            switch (*++argv[0]) {
-                case 'c':
-                    compile = *++argv;
-                    break;
-                case 'r':
-                    run = *++argv;
-                    break;
-                default:
-                    argc = -1;
-                    break;
-            }
-            --argc;
+    // file registeration
+    if (argc > 1) {
+        int fcount = dirlist(argv[1], FILES_ONLY, fnames);
+        for(int i=0; i < fcount; i++)
+            register_file(fnames[i]);
+    }
+    else {
+        printf("Files to watch:\n~ ");
+        while(_getline(stdin, new_file, MAX_FILE_NAME)) {
+            register_file(new_file);
+            printf("~ ");
         }
     }
-    if (argc != 0 || compile == NULL) {
-        usage();
-        return 1;
-    }
 
-    // get files to watch from stdin, max 100 files, small project
-    char* fnames[MAXFILES];
+    run[0] = compile[0] = args[0] =  '\0';
+    get_commands();
 
-    for (int i=0; i < MAXFILES; i++) {
-        fnames[i] = calloc(MAXFILES, MAXFNAME);
-    }
-
-    // get file list
-    // if -r program is provided, remove from list
-    // build array that holds original mod time for each file
-    printf("Files to watch:\n~ ");
-    int n;
-    struct stat st;
-    time_t mtimes[MAXFILES];
-    for (n=0; _getline(stdin, fnames[n], MAXFNAME); n++) {
-        if (run && strcmp(fnames[n], run) == 0)
-            --n;
-        else if (stat(fnames[n], &st) == -1)
-            fprintf(stderr, "%s can't access %s\n", pname, fnames[n]);
-        else
-            mtimes[n] = st.st_mtime;
-        printf("~ ");
-    }
-
-    // create string for system call
-    char compcall[100];
-    sprintf(compcall, "cls && %s", compile);
-
-    // create kill compcall
-    if (run)
-        sprintf(kill,"taskkill /im %s /f", run);
-
-    // detect file changes
-    p.pname = pname;
-    p.runcall = run;
-    p.compcall = compcall;
-
-    is_running = 0;
-    run_mutex = CreateMutex(0, FALSE, 0);
-    _beginthread(system_call, 0, 0);
+    is_running = 1;
     _beginthread(input, 0, 0);
+    _beginthread(sys_call, 0, 0);
 
+    printf("n: %d \n", n);
     while (1) {
         Sleep(1000);
         int mod = 0;
         for (int i=0; i < n; i++) {
+            struct stat st;
+            if (!fnames[i][0])
+                continue;
             stat(fnames[i], &st);
             if (mtimes[i] != st.st_mtime) {
                 mtimes[i] = st.st_mtime;
@@ -127,61 +94,113 @@ int main(int argc, char** argv)
         if (mod) {
             if (run)
                 system(kill);
-            _beginthread(system_call, 0, 0);
+            int ret = system(compile);
+            if (ret == 0)
+                _beginthread(sys_call, 0, 0);
         }
     }
+}
 
-    CloseHandle(run_mutex);
+void get_commands()
+{
+    print_commands();
+    char b[1000];
+    while(_getline(stdin, b, 1000)) {
+
+        if (strncmp(b, "c=", 2)==0)
+            sprintf(compile,"cls && %s", b+2);
+
+        else if (strncmp(b, "r=", 2)==0) {
+            strcpy(run, b+2);
+            sprintf(kill,"taskkill /im %s /f", run);
+
+            // remove the executible from watchlist 
+            for(int i=0; i < n; i++)
+                if (strcmp(fnames[i], b+2) == 0)
+                    fnames[i][0] = '\0';
+        }
+
+        else if (strncmp(b, "a=", 2)==0)
+            strcpy(args, b+2);
+
+        else if (strncmp(b, "f=", 2)==0)
+            register_file(b+2);
+        else if (strncmp(b, "s=", 2)==0)
+            system(b+2);
+        else if (strcmp(b, "done")==0)
+            break;
+        else
+            printf("wrong command\n");
+    }
+
+    if (compile[0]=='\0') {
+        printf("\nplease at least provide a compilation command\nprevious commands have been saved\n");
+        get_commands();
+    }
+}
+
+void sys_call()
+{
+    if (run[0]) {
+        clock_t start = clock(), end;
+        char b[1000];
+        sprintf(b, "cls && %s %s", run, args);
+        int ret = system(b);
+        end = clock();
+        printf("\n------------------------------------------------------\n");
+        printf("%s: %s returned %d in %.2lf(s)\n\n", pname, b, ret, (float)(end - start)/CLOCKS_PER_SEC);
+    }
+
+    print_keys();
 }
 
 void input()
 {
+    int ir;
     for(;;) {
 
-        WaitForSingleObject(run_mutex, INFINITE);
-        int ir = is_running;
-        ReleaseMutex(run_mutex);
+        int c = _getch();
 
-        if (!ir) {
-            int c =_getch();
+        if (c == 'c') {
+            system(compile);
+            print_keys();
+        }
+
+        else if (c == KEY_ESC) {
+            get_commands();
+            print_keys();
+        }
+
+
+        else if (run[0]) {
             if (c == 'q')
                 system(kill);
             else if (c == 'r') {
-                if (p.runcall) {
-                    is_running = 1; // beginthread takes too long so the loop starts again with ir begin 0, which is wrong
-                    system(kill);
-                }
-                _beginthread(system_call, 0, 0);
+                system(kill);
+                _beginthread(sys_call, 0, 0);
             }
-            else if (c == KEY_ESC) {
-                if (p.runcall)
-                    system(kill);
-                exit(0);
+            else if (c == 'e') {
+                system(kill);
+                int ret = system(compile);
+                if (ret == 0)
+                    _beginthread(sys_call, 0, 0);
             }
         }
     }
 }
 
-void system_call()
+void register_file(char* new_file)
 {
-    system(p.compcall);
+    // do not register exe files
+    if (strstr(new_file, ".exe"))
+        return;
 
-    if (p.runcall) {
-
-        WaitForSingleObject(run_mutex, INFINITE);
-        is_running = 1;
-        ReleaseMutex(run_mutex);
-
-        clock_t start = clock(), end;
-        int ret = system(p.runcall);
-        end = clock();
-        printf("\n\n------------------------------------------------------\n");
-        printf("%s: %s returned %d in %.2lf(s)\n\n", p.pname, p.runcall, ret, (float)(end - start)/CLOCKS_PER_SEC);
-        fflush(stdout);
-
-        WaitForSingleObject(run_mutex, INFINITE);
-        is_running = 0;
-        ReleaseMutex(run_mutex);
+    struct stat st;
+    if (stat(new_file, &st) == -1)
+        fprintf(stderr, "%s can't access %s\n", pname, new_file);
+    else {
+        strcpy(fnames[n], new_file);
+        mtimes[n++] = st.st_mtime;
     }
 }
 
@@ -196,22 +215,30 @@ int _getline(FILE* f, char* b, int max)
     return i;
 }
 
+void print_keys()
+{
+    printf("\nKeys\n");
+    printf("----------------------------\n");
+    printf("c   'compile'\n");
+    printf("q   'kill running program\n");
+    printf("r   'restart program'\n");
+    printf("e   'compile and restart\n");
+    printf("ESC 'exit Keys mode and get back to commands'\n\n");
+    printf("Listening for changes...\n\n");
+}
+
+void print_commands()
+{
+    printf("\nCommands\n");
+    printf("----------------------------\n");
+    printf("c='compilation command' (ex: make)\n");
+    printf("r='program to run' (ex: demo.exe)\n");
+    printf("a='program flags' (ex: -f file.txt)\n");
+    printf("s='shell command'\n");
+    printf("f='new file to watch'\n");
+    printf("type 'done' to continue \n\n");
+}
+
 void usage()
 {
-    char str[] = "\n\
-  FLAGS\n\
-    -C    - compilation command ex: make, 'gcc main.c'\n\
-    -r    - (optional) program to run after compilation, including the file extension\n\
-\n\
-  KEYS\n\
-    'ESC' - kill filewatch\n\
-    'q'   - if -r is provided, kill the -r program\n\
-    'r'   - restart -r program by terminating the program by name\n\
-            CAUTION: name clashing with other programs may occur\n\
-\n\
-  TIP\n\
-    use ' fl | grep -v ' to filter out unwanted files\n\
-    Programs that use stdin will not work because a single console window is shared with filewatch\n\
-    DO NOT WATCH EXECUTIBLES\n";
-    fprintf(stderr, "%s", str);
 }
